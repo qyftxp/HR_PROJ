6b7a4804e4f4f34221be6f367bfd0834ddd94f40
qyftxp <1061106027@qq.com>
2018-08-23 11:31:46 +0800

    kk

--- src/main/java/com/yc/controllers/UserController.java
+++ src/main/java/com/yc/controllers/UserController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +16,8 @@ import com.yc.bean.User;
 import com.yc.biz.UserBiz;
 import com.yc.web.model.JsonModel;
 
-@RestController    //类注解同时使用@Controller 和@ResponseBody
+//@RestController    //类注解同时使用@Controller 和@ResponseBody
+@Controller
 public class UserController {
 	@Resource(name="useBizImpl")
 	private UserBiz userBiz;
@@ -39,10 +41,10 @@ public class UserController {
 	@RequestMapping("/user_login.action")
 	//因为登录后是要直接到list.jsp页做显示,且login.jsp页是通过submit提交的,即不支持ajax,不用回送json数据,而应回送是一个页面名字,
 	//这样springmvc的视图解析器就可以运行了
-	public ModelAndView login(User user,HttpServletRequest request,HttpSession session){
+	public String login(User user,HttpServletRequest request,HttpSession session){
 		//为什么有三个参数?因为name,password是User对象中有的,但zccode是在类中没有的饿,所以要通过request获取
 		//另外,sRand是存在session中的,所以还需要HttpSession
-		ModelAndView mav=new ModelAndView();
+		/*ModelAndView mav=new ModelAndView();*/
 		String zccode=request.getParameter("zccode");
 		String sRand=session.getAttribute("sRand").toString();
 		if(! sRand.equals(zccode)){
@@ -52,14 +54,14 @@ public class UserController {
 			user=userBiz.login(user);
 			if(user!=null){
 				session.setAttribute("user", user);
-				mav.setViewName("main");
-				return mav;
+				//mav.setViewName("main");
+				return "main";
 			}else{
 				request.setAttribute("errormsg","用户名或密码错误");
 			}
 		}
-		mav.setViewName("login");
-		return mav;
+		//mav.setViewName("login");
+		return "login";
 	}
 	
 }
